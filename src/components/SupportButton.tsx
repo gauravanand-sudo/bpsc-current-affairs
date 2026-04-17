98d302afa2f3f55bd4c72760b2c3ab7f4b20749d
gauravanand-iitp <gaurav_2503mth128@iitp.ac.in>
2026-04-18 02:14:45 +0530

    Hide Feeling sad button on support chat page

--- src/components/SupportButton.tsx
+++ src/components/SupportButton.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function SupportButton() {
+  const pathname = usePathname();
+  if (pathname === "/support") return null;
+
   return (
     <div style={{
       position: "fixed",
