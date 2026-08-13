c26ee0ce9cf64289b49194f968ea30b2fe7d0f93
GauravAnand_IITP <ga.oct.2024@gmail.com>
2026-08-14 00:57:53 +0530

    Load CA institutional overrides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import SiteFooter from "@/components/SiteFooter";
 import SupportButton from "@/components/SupportButton";
 import "./globals.css";
 import "./institutional-theme.css";
+import "./ca-legacy-theme.css";
 
 const bodyFont = Plus_Jakarta_Sans({ variable: "--font-body", subsets: ["latin"] });
 const displayFont = Space_Grotesk({ variable: "--font-display", subsets: ["latin"], weight: ["500", "700"] });
